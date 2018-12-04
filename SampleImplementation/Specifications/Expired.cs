e5fbffab13b8b08b963deb8a74a941a6c27d1325
Erick Bressani <42618008+erickbress@users.noreply.github.com>
2018-12-04 20:15:16 -0200

    Update Expired.cs

--- SampleImplementation/Specifications/Expired.cs
+++ SampleImplementation/Specifications/Expired.cs
@@ -6,7 +6,7 @@ namespace SampleImplementation
     public class Expired : ISpecification<Lot>, IHasErrorMessageWhenExpectingFalse<Lot>
     {
         public string GetErrorMessageWhenExpectingFalse(Lot contract)
-            => $"Lot {contract.LotNumber} is expired and connot be used";
+            => $"Lot {contract.LotNumber} is expired and cannot be used";
 
         public bool Validate(Lot contract)
             => contract.ExpirationDate.Date <= DateTime.Now.Date;
