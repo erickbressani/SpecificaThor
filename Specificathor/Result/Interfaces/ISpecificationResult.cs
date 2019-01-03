980423b526190fee1d1fd6652f0f84c8012fcb67
erick <erick@hanoog.com.br>
2019-01-03 09:30:52 -0200

    Minor adjustment

--- Specificathor/Result/Interfaces/ISpecificationResult.cs
+++ Specificathor/Result/Interfaces/ISpecificationResult.cs
@@ -5,7 +5,7 @@
         bool IsValid { get; }
         string ErrorMessage { get; }
         int TotalOfErrors { get; }
-        TCandidate Candidate { get;  }
+        TCandidate Candidate { get; }
 
         bool HasError<TSpecification>() where TSpecification : ISpecification<TCandidate>;
     }
